062e7ddbdb54d7dd69239ab6ff618a2007f3230c
Hector Sanchez <hsanchez7934@gmail.com>
2017-09-18 19:18:32 -0600

    add testing files

--- test/App.test.js
+++ test/App.test.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import App from '../lib/components/App/App.js';
 import { shallow, mount } from 'enzyme';
+import MockData from '../Mockdata.js';
+
 
 describe('App Component', () => {
-
+  const weather = MockData.current_observation
 })
