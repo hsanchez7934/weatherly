a57716b959160de5cfadbaa5bb0d655bd849212f
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2017-09-23 14:57:10 -0600

    complete one hour test with Nathaniel's change request

--- test/CurrentWeather.test.js
+++ test/CurrentWeather.test.js
@@ -9,18 +9,21 @@ describe('Current Weather', () => {
   let wrapper;
   let data = new DataProps(MockData);;
 
+
   it('should render one current wether card', () => {
     wrapper = shallow(<CurrentWeather currentObservation={[data]}/>);
     let weatherCard = wrapper.find('.weather-card');
     let weatherCardOne = weatherCard.at(0);
 
     expect(weatherCard.length).toEqual(1)
-  })
+  });
+
 
   it('should have a city', () => {
     let cityName = wrapper.find('city');
     expect(cityName).toBeTruthy();
   });
+
 
   it('should render correct city information', () => {
     wrapper = shallow(<CurrentWeather currentObservation={[data]}/>);
@@ -30,40 +33,46 @@ describe('Current Weather', () => {
     expect(city.text()).toEqual('San Francisco, CA');
   });
 
+
   it('should have description of the weather', () => {
     let currentDate = wrapper.find('description');
     expect(currentDate).toBeTruthy();
   });
+
 
   it('should render correct weather information', () => {
     wrapper = shallow(<CurrentWeather currentObservation={[data]}/>);
     let weatherCard = wrapper.find('.weather-card');
     let description = weatherCard.find('.description')
 
-    console.log(description.text())
-
     expect(description.text()).toEqual('Clear');
+    // console.log(description.text())
   });
+
 
   it('should render icon', () => {
     let currentDate = wrapper.find('main-icon');
     expect(currentDate).toBeTruthy();
   });
 
+
   it('should display a low temperature', () => {
     let currentTemp = wrapper.find('low');
     expect(currentTemp).toBeTruthy();
   });
+
 
   it('should display a high temperature', () => {
     let currentTemp = wrapper.find('high');
     expect(currentTemp).toBeTruthy();
   });
 
+
   it('should display a current temperature', () => {
     let currentTemp = wrapper.find('current');
     expect(currentTemp).toBeTruthy();
   });
+
 
   it('should provide a summary of the weather conditions', () => {
     let currentDesc = wrapper.find('summary');
