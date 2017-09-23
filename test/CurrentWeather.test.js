125b3e6663c859e28e267662cd6a1aebc722cebe
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2017-09-23 13:48:24 -0600

    current weather testing with text()

--- test/CurrentWeather.test.js
+++ test/CurrentWeather.test.js
@@ -1,104 +1,73 @@
 import React, { Component } from 'react';
 import App from '../lib/components/App/App.js';
 import { shallow, mount } from 'enzyme';
-import MockData from '../Mockdata.js';
-import currentWeather from '../lib/components/Current/CurrentWeather.js';
+import MockData from '../mockdata.js';
+import CurrentWeather from '../lib/components/Current/CurrentWeather.js';
 import DataProps from '../lib/components/DataProps.js';
 
 describe('Current Weather', () => {
   let wrapper;
-  let data;
+  let data = new DataProps(MockData);;
 
-  beforeEach(() => {
-    data = new DataProps(MockData);
-    wrapper = shallow(<currentWeather currentObservation={data}/>);
-  });
+  it('should render one current wether card', () => {
+    wrapper = shallow(<CurrentWeather currentObservation={[data]}/>);
+    let weatherCard = wrapper.find('.weather-card');
+    let weatherCardOne = weatherCard.at(0);
 
-  it.skip('should exist', () => {
-    expect(wrapper).toBeDefined();
-    // console.log(data);
-    // console.log(wrapper.debug());
-var para =wrapper.find('.city');
-var paraTxt =para.text();
-console.log(paraTxt);
-  });
+    expect(weatherCard.length).toEqual(1)
+  })
 
-  it.skip('should have a city', () => {
+  it('should have a city', () => {
     let cityName = wrapper.find('city');
     expect(cityName).toBeTruthy();
   });
 
-  it.skip('should have description of the weather', () => {
+  it('should render correct city information', () => {
+    wrapper = shallow(<CurrentWeather currentObservation={[data]}/>);
+    let weatherCard = wrapper.find('.weather-card');
+    let city = weatherCard.find('.city')
+
+    expect(city.text()).toEqual('San Francisco, CA');
+  });
+
+  it('should have description of the weather', () => {
     let currentDate = wrapper.find('description');
     expect(currentDate).toBeTruthy();
   });
 
-  it.skip('should render icon', () => {
+  it('should render correct weather information', () => {
+    wrapper = shallow(<CurrentWeather currentObservation={[data]}/>);
+    let weatherCard = wrapper.find('.weather-card');
+    let description = weatherCard.find('.description')
+
+    console.log(description.text())
+
+    expect(description.text()).toEqual('Clear');
+  });
+
+  it('should render icon', () => {
     let currentDate = wrapper.find('main-icon');
     expect(currentDate).toBeTruthy();
   });
 
-  it.skip('should display a low temperature', () => {
+  it('should display a low temperature', () => {
     let currentTemp = wrapper.find('low');
     expect(currentTemp).toBeTruthy();
   });
 
-  it.skip('should display a high temperature', () => {
+  it('should display a high temperature', () => {
     let currentTemp = wrapper.find('high');
     expect(currentTemp).toBeTruthy();
   });
 
-  it.skip('should display a current temperature', () => {
+  it('should display a current temperature', () => {
     let currentTemp = wrapper.find('current');
     expect(currentTemp).toBeTruthy();
   });
 
-  it.skip('should provide a summary of the weather conditions', () => {
+  it('should provide a summary of the weather conditions', () => {
     let currentDesc = wrapper.find('summary');
     expect(currentDesc).toBeTruthy();
-  });
-
-})
-
-describe('Seven Hour Weather', () => {
-  let wrapper;
-
-  // beforeEach(() => {
-  //   wrapper = mount(<currentWeather />);
-  // });
-
-  it.skip('should exist', () => {
-    const data = new DataProps(MockData);
-    const dataCity =data.city;
-    console.log(dataCity);
-
-    const newWrapper = shallow(<currentWeather />);
-    // console.log(newWrapper.debug());
-
-    // expect(newWrapper.contains(<p className='city'>San Francisco</p>)).toEqual(true);
-    // expect(newWrapper.contains(<p className='city'>San Francisco</p>)).toEqual(true);
-
-
-  });
-
-  it.skip('should have a time', () => {
-    let currentDate = wrapper.find('seven-hour-time');
-    expect(currentDate).toBeTruthy();
-  });
-
-  it.skip('should display a current temperature', () => {
-    let currentTemp = wrapper.find('seven-hour-condition');
-    expect(currentTemp).toBeTruthy();
-  });
-
-  it.skip('should render icon', () => {
-    let currentDate = wrapper.find('icon');
-    expect(currentDate).toBeTruthy();
-  });
-
-  it.skip('should have description of the weather', () => {
-    let currentDate = wrapper.find('description');
-    expect(currentDate).toBeTruthy();
   });
 
 })
